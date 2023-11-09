da8be09705c2be629ad903adc6c2393cb4f990b4
minkhantthu <minkhantthu@192.168.31.215>
2023-11-09 17:51:06 +0630

    jpa element collections

--- 05-jpa-fieldMapping/src/main/java/com/jdc/mkt/entity/Category.java
+++ 05-jpa-fieldMapping/src/main/java/com/jdc/mkt/entity/Category.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Getter;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import javax.persistence.Column;
 
 @Getter
 @Setter
@@ -21,10 +23,16 @@ public class Category implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	@Id
-	@GeneratedValue(generator = "product_gen")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	//@GeneratedValue(generator = "product_gen")
 	//@SequenceGenerator(name = "category_gen")
 	private int id;
 	@NonNull
+	@Column(name = "cat_name",
+	unique = true,
+	nullable = false,
+	length = 45)
 	private String name;
+	@Column(columnDefinition = "tinyint(1) not null default 1")
 	private boolean active;
 }
--- 05-jpa-fieldMapping/src/test/java/com/jdc/mkt/test/CategoryTest.java
+++ 05-jpa-fieldMapping/src/test/java/com/jdc/mkt/test/CategoryTest.java
@@ -3,18 +3,16 @@ package com.jdc.mkt.test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import com.jdc.mkt.entity.Category;
-
 public class CategoryTest extends JpaFactory{
 
 	
 	@ParameterizedTest
 	@ValueSource(strings="Foods")
 	void testInsert(String name) {
-		var category = new Category(name);	
-		em.getTransaction().begin();
-		em.persist(category);
-		em.getTransaction().commit();
+//		var category = new Category(name);	
+//		em.getTransaction().begin();
+//		em.persist(category);
+//		em.getTransaction().commit();
 		
 	}
 	
